89613b9db1768c39a05fa0307db8f44b95646bab
Jimmy01240397 <57281249+Jimmy01240397@users.noreply.github.com>
2021-08-05 17:04:18 +0800

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -7,9 +7,6 @@ import time
 
 from flask import Flask,request,redirect,Response
 
-#SITE_NAME = "https://www.hpool.com"
-#HOST_NAME = "www.hpool.com"
-
 app = Flask(__name__)
 
 print("aaa")
@@ -17,28 +14,17 @@ print("aaa")
 
 @app.route('/',methods=['GET','POST','DELETE'])
 def host():
-#    global SITE_NAME
     return proxy("", request)
-
-
-#@app.route('/')
-#def test():
-#    print("aaa")
-#    return 'runing'
-
 
 @app.route('/<path:path>',methods=['GET','POST','DELETE'])
 def hostpath(path):
-    #print(request.url)
-#    global SITE_NAME
-#    return proxy(f'{SITE_NAME}/{path}', request)
     return proxy(path, request)
 
 
 def proxy(url, request):
     print("\n%s %s with headers: %s" % (request.method, url, request.headers))
     inheaders = dict(request.headers)
-    hostfor = inheaders['X-Forwarded-Host']
+    hostfor = inheaders['Host']
     r = make_request(f"https://{hostfor}/{url}", request.method, dict(inheaders), request.form)
     excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
     headers = [(name, value) for (name, value) in r.raw.headers.items() if name.lower() not in excluded_headers]
@@ -48,29 +34,12 @@ def proxy(url, request):
     out = Response(r.content, r.status_code, headers)
     return out
 
-#    if request.method=='GET':
-#        resp = requests.get(url)
-#        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-#        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
-#        response = Response(resp.content, resp.status_code, headers)
-#        return response
-#    elif request.method=='POST':
-#        resp = requests.post(url,json=request.get_json())
-#        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-#        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
-#        response = Response(resp.content, resp.status_code, headers)
-#        return response
-#    elif request.method=='DELETE':
-#        resp = requests.delete(url).content
-#        response = Response(resp.content, resp.status_code, headers)
-#        return response
-
 def make_request(url, method, headers={}, data=None):
     # Fetch the URL, and stream it back
-    headers['Host'] = headers['X-Forwarded-Host']
-    del headers['X-Forwarded-For']
-    del headers['X-Forwarded-Server']
-    del headers['X-Forwarded-Host']
+    if method == 'POST':
+        if headers['Content-Type'] == 'application/json':
+            print("Sending %s %s with headers: %s and data %s\n" % (method, url, headers, request.get_data()))
+            return requests.request(method, url, params=request.args, stream=True, headers=headers, allow_redirects=False, json=json.loads(request.get_data()))
     print("Sending %s %s with headers: %s and data %s\n" % (method, url, headers, data))
     return requests.request(method, url, params=request.args, stream=True, headers=headers, allow_redirects=False, data=data)
 
